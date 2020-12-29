59ecf7db58f411041bb30bbc9af8f502b3dc4736
Jim Remsik <jim@beflagrant.com>
2020-12-29 14:11:04 -0600

    Make Hotwire::Debug a Rails engine

--- lib/hotwire/debug.rb
+++ lib/hotwire/debug.rb
@@ -2,7 +2,6 @@ require "hotwire/debug/version"
 
 module Hotwire
   module Debug
-    class Error < StandardError; end
-    # Your code goes here...
+    class Engine < ::Rails::Engine; end
   end
 end
