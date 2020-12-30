e3631c0c643ba13c11bdf4de9bd8a467b764b7ff
Jim Remsik <jim@beflagrant.com>
2020-12-30 16:51:50 -0600

    Integrating with hotwire chat app revealed some sharp edges

--- vendor/assets/javascripts/hotwire-debug.js
+++ vendor/assets/javascripts/hotwire-debug.js
@@ -14,26 +14,38 @@ var turboEvents = [
 
 turboEvents.forEach(function(turboEvent) {
   document.addEventListener(turboEvent, function() {
-    console.log(turboEvent);
-    handleChange();
+    if (showEvents()) {
+      console.log(turboEvent);
+    }
+    if (showFrameChanges()) {
+      handleChange();
+    }
   });
 })
 
-function createToggle() {
-  let div = document.createElement("div");
-  div.innerHTML = '<svg viewBox="0 0 196 196" xmlns="http://www.w3.org/2000/svg"> <style> path { fill: #000000; } @media (prefers-color-scheme: dark) { path { fill: #ffffff; } } </style> <path d="m16.38 184.8 109.46-44.72-35.27-3.31 76.73-66.59-44.33-4.16 53.9-55.53-136.52 70.03 54.89 6.44-67.32 54.38 30.07 2.97z"/> </svg>';
-  div.id = "hotwire-toggle";
-  return div
+function showEvents () {
+  return document.body.hasAttribute('data-hotwire-debug')
+}
+
+function showFrameChanges () {
+  return document.body.hasAttribute('data-hotwire-debug')
 }
 
 function handleChange() {
   document.querySelectorAll('turbo-frame > div, turbo-frame > form').forEach(function(target) { target.setAttribute('data-turbo-frame-id',target.closest('turbo-frame').id) });
 }
 
-window.addEventListener("load", function() {
-  var toggle = createToggle();
-  body.append(toggle);
-  toggle.addEventListener("click", function() {
-    body.toggleAttribute("data-hotwire-debug");
-  })
-})
+// This needs to happen more than just on load.
+window.onload = function() {
+  let hotwireToggle = document.getElementById('hotwire-toggle');
+  if (hotwireToggle == null) {
+    hotwireToggle = document.createElement("div");
+    hotwireToggle.innerHTML = '<svg viewBox="0 0 196 196" xmlns="http://www.w3.org/2000/svg"> <style> path { fill: #000000; } @media (prefers-color-scheme: dark) { path { fill: #ffffff; } } </style> <path d="m16.38 184.8 109.46-44.72-35.27-3.31 76.73-66.59-44.33-4.16 53.9-55.53-136.52 70.03 54.89 6.44-67.32 54.38 30.07 2.97z"/> </svg>';
+    hotwireToggle.id = "hotwire-toggle";
+
+    document.body.append(hotwireToggle);
+    hotwireToggle.addEventListener("click", function() {
+      document.body.toggleAttribute("data-hotwire-debug");
+    });
+  }
+}
